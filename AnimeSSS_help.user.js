c65ec1d32e5cb06651f050e71b854dd836205ffd
Grizordin <91417760+Grizordin@users.noreply.github.com>
2026-06-26 16:07:13 +0500

    Bump script version to 2.39

--- AnimeSSS_help.user.js
+++ AnimeSSS_help.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AnimeSSS помощник
 // @namespace    http://tampermonkey.net/
-// @version      2.38
+// @version      2.39
 // @description  Комбайн функций для animesss.tv/com
 // @author       BETEP_B_TYMAHE
 // @match        https://animesss.tv/*
